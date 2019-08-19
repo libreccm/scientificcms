ba30f3d60ed745192f93e76dcf8d37fcead55f78
Jens Pelzetter <jens@jp-digital.de>
2019-08-19 20:26:55 +0200

    Assets for the publications modulAssets for the publications modulee

---
+++ sci-publications/src/main/java/org/scientificcms/SciPublicationsConstants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.scientificcms;
+
+/**
+ *
+ * @author <a href="mailto:jens.pelzetter@googlemail.com">Jens Pelzetter</a>
+ */
+public class SciPublicationsConstants {
+    
+    public static final String DB_SCHEMA = "SCI_PUBLICATIONS";
+    
+}
---
+++ sci-publications/src/test/java/org/libreccm/security/SecurityEntitiesPrefabProvider.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.libreccm.security;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+/**
+ *
+ * @author <a href="mailto:jens.pelzetter@googlemail.com">Jens Pelzetter</a>
+ */
+public final class SecurityEntitiesPrefabProvider {
+
+    private SecurityEntitiesPrefabProvider() {
+        // Nothing
+    }
+
+    public static void addPrefabEntities(final EqualsVerifier<?> verifier) {
+
+        final Role role1 = new Role();
+        role1.setRoleId(2001);
+        role1.setName("role1");
+
+        final Role role2 = new Role();
+        role2.setRoleId(2002);
+        role2.setName("role2");
+
+        verifier.withPrefabValues(Role.class, role1, role2);
+
+        final Group group1 = new Group();
+        group1.setPartyId(3001);
+        group1.setName("group1");
+
+        final Group group2 = new Group();
+        group2.setPartyId(3002);
+        group2.setName("group2");
+
+        verifier.withPrefabValues(Group.class, group1, group2);
+
+        final User user1 = new User();
+        user1.setPartyId(4001);
+        user1.setName("user1");
+
+        final User user2 = new User();
+        user2.setPartyId(4002);
+        user2.setName("user2");
+
+        verifier.withPrefabValues(User.class, user1, user2);
+    }
+
+}
